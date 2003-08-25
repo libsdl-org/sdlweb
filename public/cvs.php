ac84fcbbcaf52729e3432a6a759c51ccb273a56d
SDL Web Management <slouken@libsdl.org>
2003-08-25 03:16:26 +0000

    *** empty log message ***

--- public/cvs.php
+++ public/cvs.php
@@ -38,9 +38,15 @@ You can also browse the CVS repository online at:
 <blockquote>
   <p><font color="#414141"><strong>
     Source snapshot version 1.2 (
-Sat Aug  9
+Sat Aug 23
   )</p>
   <p><a href="cvs/SDL-1.2.tar.gz">SDL-1.2.tar.gz</a>
+  </p>
+  <p>Changes for Sat Aug 23: <br>
+     <ul>
+	<LI> 1.2.6: Fixed use of SDL with XInitThreads()
+	<LI> 1.2.6: Added MMX and 3DNow! optimized alpha blitters (thanks Stephane!)
+     </ul>
   </p>
   <p>Changes for Sat Aug 9: <br>
      <ul>
