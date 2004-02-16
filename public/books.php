8c5001001aaadfaab3ee54c0e6976c9952b0b0d0
SDL Web Management <slouken@libsdl.org>
2004-02-16 02:00:46 +0000

    Fixed typos

--- public/books.php
+++ public/books.php
@@ -8,25 +8,25 @@ Books relating to SDL
 
 <blockquote>
      Alberto Garc&iacute;a Serrano - December 2003<br>
-     <b><a href="http://www.ediversitas.com/public-gen.php?isbn=8495836084">Programaci&oacute;n de videojuegos en SDL</a></b<br>
+     <b><a href="http://www.ediversitas.com/public-gen.php?isbn=8495836084">Programaci&oacute;n de videojuegos en SDL</a></b><br>
      Excellent coverage of using SDL in the development of video games.
 </blockquote>
 
 <blockquote>
      Ernest Pazera - November 2002<br>
-     <b><a href="http://www.gamedev.net/columns/books/bookdetails.asp?productid=206">Focus On SDL</a></b<br>
+     <b><a href="http://www.gamedev.net/columns/books/bookdetails.asp?productid=206">Focus On SDL</a></b><br>
      A good introduction to SDL, SDL_image, SDL_mixer, SDL_ttf, and SDL_net.
 </blockquote>
 
 <blockquote>
      Ron Penton - November 2002<br>
-     <b><a href="http://www.gamedev.net/columns/books/bookdetails.asp?productid=197">Data Structions for Game Programmers</a></b<br>
+     <b><a href="http://www.gamedev.net/columns/books/bookdetails.asp?productid=197">Data Structions for Game Programmers</a></b><br>
      Coverage of data structures used in game development, with SDL examples.
 </blockquote>
 
 <blockquote>
      Loki Software, Inc. with John R. Hall - August 2001<br>
-     <b><a href="http://www.nostarch.com/plg.htm">Programming Linux Games</a></b<br>
+     <b><a href="http://www.nostarch.com/plg.htm">Programming Linux Games</a></b><br>
      An introduction to programming games on Linux using the SDL library.
 </blockquote>
 
