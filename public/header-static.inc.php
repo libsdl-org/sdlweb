3f21fb0bf3b6b6bf317051fae2dc28ab412a5482
Ga?tan de Menten <ged@bugfactory.org>
2005-01-18 01:43:19 +0000

    handle an unavailable database (slightly) more gracefully

---
+++ public/header-static.inc.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+<title>Simple DirectMedia Layer</title>
+<meta content="text/html; charset=iso-8859-1" http-equiv="content-type">
+<link rel="stylesheet" href="/sdlweb.css" type="text/css">
+</head>
+
+<body>
+
+<!-- Title -->
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+<tr>
+	<td background="/images/titlebg.png" colspan="2"><a href="/index.php"><img src="/images/title.png" width="181" height="73" alt="SDL" border="0"></a></td>
+	<td background="/images/titlebg.png" rowspan="2" valign="bottom">
+
+	<table cellpadding="0" cellspacing="0" border="0" width="100%">
+	<tr>
+	<form method="post" action="/search.php">
+		<td><img src="/images/searchbarleft.png" alt="" width="5" height="30" ></td>
+		<td valign="middle" background="/images/searchbarbg.png"><input type="text" name="keywords" size="12"></td>
+		<td><input type="image" value="Search Site" src="/images/go.png"></td>
+	</form>
+	</tr>
+	</table>
+
+	</td>
+</tr>
+<!-- End Title -->
+
+<!-- Top Bar -->
+<tr>
+	<td background="/images/topbarbg.png" width="100%"><a href="http://www.libsdl.de/"><img src="/images/germanbutton.png" alt="German Site" width="82" height="20"></a></td>
+	<td background="/images/topbarbg.png" align="right"><img src="/images/searchsite.png" alt="Search Site:" width="58" height="20"></td>
+</tr>
+</table>
+<!-- End Top Bar -->
+
+<!-- Main -->
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+<tr>
+
+	<!-- Side Bar-->
+	<td valign="top">
+
+	<!-- Main -->
+	<img src="/images/main.png" width="180" height="31" alt="Main"><br>
+	<ul class="menu">
+	<li><a href="/index.php">About</a></li>
+	<li class="unavailable">News</li>
+	<li><a href="/license.php">Licensing</a></li>
+	<li><a href="http://www.cafepress.com/libsdl">Merchandise</a></li>
+	<li><a href="/credits.php">Credits</a></li>
+	<li><a href="mailto:slouken@libsdl.org">Feedback</a></li>
+	</ul>
+	<!-- End Main -->
+
+	<!-- Documentation -->
+	<img src="/images/documentation.png" width="180" height="31" alt="Documentation"><br>
+	<ul class="menu">
+	<li><a href="/docs.php">Downloadable</a></li>
+	<li><a href="/intro.php">Introduction</a></li>
+	<li><a href="/tutorials.php">Tutorials</a></li>
+	<li><a href="/articles.php">Articles</a></li>
+	<li><a href="/books.php">Books</a></li>
+	<li><a href="http://www.libsdl.org/cgi/docwiki.cgi/">Doc Wiki</a></li>
+	<li><a href="http://sdldoc.csn.ul.ie/">Doc Project</a></li>
+	<li class="unavailable">FAQs</li>
+	<li><a href="/opengl/index.php">OpenGL</a></li>
+	<li><a href="/mailing-list.php">Mailing Lists</a></li>
+	<li><a href="http://news.gmane.org/thread.php?group=gmane.comp.lib.sdl">Newsgroup</a></li>
+	</ul>
+	<!-- End Documentation -->
+
+	<!-- Download -->
+	<img src="/images/download.png" width="180" height="32" alt="Download"><br>
+	<ul class="menu">
+	<li><a href="/download-1.0.php">SDL 1.0</a></li>
+	<li><a href="/download-1.2.php">SDL 1.2</a></li>
+	<li><a href="/cvs.php">SDL CVS</a></li>
+	<li class="unavailable">Games</li>
+	<li class="unavailable">Demos</li>
+	<li class="unavailable">Applications</li>
+	<li class="unavailable">Libraries</li>
+	<li><a href="/languages.php">Languages</a></li>
+	<li><a href="http://www.libsdl.de/logo/">Logos</a></li>
+	</ul>
+	<!-- End Download -->
+
+	<!-- Other -->
+	<img src="/images/other.png" width="180" height="32" alt="Other"><br>
+	<ul class="menu">
+	<li><a href="/finances.php">SDL Finances</a></li>
+	</ul>
+	<!-- End Other -->
+
+	</td>
+	<!-- End Side Bar -->
+
+	<!-- Body -->
+	<td width="100%" valign="top">
+
+	<!-- Body Table -->
+	<table cellpadding="0" cellspacing="0" border="0" width="100%">
+	<tr>
+
+		<!-- Left Body -->
+		<td width="80%">
+			<table cellpadding="16" cellspacing="0">
+			<tr>
+				<td>
