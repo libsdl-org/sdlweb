0f37f545cd0f6743dc4415a1bf3011699e37a236
Ga?tan de Menten <ged@bugfactory.org>
2005-01-19 19:39:55 +0000

    added favicon

--- public/header.inc.php
+++ public/header.inc.php
@@ -4,7 +4,8 @@
 <head>
 <title>Simple DirectMedia Layer</title>
 <meta content="text/html; charset=iso-8859-1" http-equiv="content-type">
-<link rel="stylesheet" href="/sdlweb.css" type="text/css">
+<link rel="stylesheet" type="text/css" href="/sdlweb.css">
+<link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
 </head>
 
 <body>
