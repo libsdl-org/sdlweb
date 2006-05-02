4bea5fca57ba7f702a7809062a1c86e07ff24b1a
SDL Web Management <slouken@libsdl.org>
2006-05-02 15:17:47 +0000

    Added instructions for copying SDL_config.h

--- public/svn.php
+++ public/svn.php
@@ -7,39 +7,46 @@ SDL source snapshots
 </strong></font></h1>
 
 <p>
-<strong>Warning: This code is unstable and may be broken at any time</strong>
-<br>This code is for testing only - DO NOT REDISTRIBUTE!
+The latest development version of SDL is available via Subversion.
+Subversion allows you to get up-to-the-minute fixes and enhancements;
+as a developer works on a source tree, you can use svn to mirror that
+source tree instead of waiting for an official release. Please look
+at the Subversion website ( http://subversion.tigris.org/ ) for more
+information on using svn, where you can also download software for
+MacOS, Windows, and Unix systems.
 </p>
-<hr>
-
 <p>
-Getting and building SDL via
-<a href="http://subversion.tigris.org/">Subversion</a>:
-</p>
 <blockquote>
 <pre>
 svn checkout svn://libsdl.org/trunk/SDL
-cd SDL; ./autogen.sh; ./configure; make; make install
-# periodically run "make distclean; svn update"
 </pre>
 </blockquote>
+</p>
 <p>
-You can also browse the repository online at:
-<br>
-<a href="http://www.libsdl.org/wsvn"
-        >http://www.libsdl.org/wsvn</a>
+  If you are building SDL with an IDE, you will need to copy the file
+  include/SDL_config.h.default to include/SDL_config.h before building.
+</p>
+<p>
+  If you are building SDL via configure, you will need to run autogen.sh
+  before running configure.
+</p>
+<p>
+There is a web interface to the subversion repository at:<br>
+<a href="http://www.libsdl.org/cgi/viewvc.cgi">
+         http://www.libsdl.org/cgi/viewvc.cgi</a>
 </p>
 
 <hr>
 
+<p><font color="#414141">
+Source snapshot for SDL 1.2 (Updated
+Thu Apr 27
+)</p>
 <blockquote>
-  <p><font color="#414141"><strong>
-    Source snapshot for SDL 1.2 (Updated
-Wed Apr 26
-  )</p>
- <a href="tmp/SDL-1.2.tar.gz">SDL-1.2.tar.gz</a><br/>
- <a href="tmp/SDL-1.2.zip">SDL-1.2.zip</a>
+<a href="tmp/SDL-1.2.tar.gz">SDL-1.2.tar.gz</a><br/>
+<a href="tmp/SDL-1.2.zip">SDL-1.2.zip</a>
 </blockquote>
+<strong>Note: These are not official releases and may be unstable!</strong>
 <hr>
 <?PHP
  include ("footer.inc.php");
