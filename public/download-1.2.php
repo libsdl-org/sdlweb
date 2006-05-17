64d7cf1df7ee6e18382a6e4c46db1fb42df5a83b
SDL Web Management <slouken@libsdl.org>
2006-05-17 08:45:53 +0000

    SDL 1.2.10 release!

--- public/download-1.2.php
+++ public/download-1.2.php
@@ -2,7 +2,7 @@
  include ("../include/login.inc.php");
  include ($header_filename);
 ?>
-<h1>SDL version 1.2.9 (stable)</h1>
+<h1>SDL version 1.2.10 (stable)</h1>
 <a href="release/changes-1.2.html">ChangeLog</a>
 
 <h2>GPG Signature:</h2>
@@ -27,18 +27,18 @@ For more information about public key signatures, see
 
 <h2>Source Code:</h2>
 <blockquote>
-     <a href="release/SDL-1.2.9.tar.gz"
-                     >SDL-1.2.9.tar.gz</a>
-     - <a href="release/SDL-1.2.9.tar.gz.sig">GPG signed</a><br>
-     <a href="release/SDL-1.2.9-1.src.rpm"
-                     >SDL-1.2.9-1.src.rpm</a>
-     - <a href="release/SDL-1.2.9-1.src.rpm.sig">GPG signed</a><br>
-     <a href="release/SDL-1.2.9.zip"
-                     >SDL-1.2.9.zip</a>
-     - <a href="release/SDL-1.2.9.zip.sig">GPG signed</a><br>
-     <a href="release/SDL-1.2.9.sea.bin"
-                     >SDL-1.2.9.sea.bin</a>
-     - <a href="release/SDL-1.2.9.sea.bin.sig">GPG signed</a><br>
+     <a href="release/SDL-1.2.10.tar.gz"
+                     >SDL-1.2.10.tar.gz</a>
+     - <a href="release/SDL-1.2.10.tar.gz.sig">GPG signed</a><br>
+     <a href="release/SDL-1.2.10-1.src.rpm"
+                     >SDL-1.2.10-1.src.rpm</a>
+     - <a href="release/SDL-1.2.10-1.src.rpm.sig">GPG signed</a><br>
+     <a href="release/SDL-1.2.10.zip"
+                     >SDL-1.2.10.zip</a>
+     - <a href="release/SDL-1.2.10.zip.sig">GPG signed</a><br>
+     <a href="release/SDL-1.2.10.sea.bin"
+                     >SDL-1.2.10.sea.bin</a>
+     - <a href="release/SDL-1.2.10.sea.bin.sig">GPG signed</a><br>
 </blockquote>
 
 <h2>Runtime Libraries:</h2>
@@ -46,13 +46,13 @@ For more information about public key signatures, see
 	<p><span class="title">
 		Linux:
 		</span><br>
-		<a href="release/SDL-1.2.9-1.i386.rpm"
-		                >SDL-1.2.9-1.i386.rpm</a><br>
+		<a href="release/SDL-1.2.10-1.i386.rpm"
+		                >SDL-1.2.10-1.i386.rpm</a><br>
+		<a href="release/SDL-1.2.10-1.mipsel.rpm"
+		                >SDL-1.2.10-1.mipsel.rpm</a> (Playstation 2)<br>
 <!-- Not available yet
-		<a href="release/SDL-1.2.9-1.mipsel.rpm"
-		                >SDL-1.2.9-1.mipsel.rpm</a> (Playstation 2)<br>
-		<a href="release/SDL-1.2.9-1.ppc.rpm"
-		                >SDL-1.2.9-1.ppc.rpm</a><br>
+		<a href="release/SDL-1.2.10-1.ppc.rpm"
+		                >SDL-1.2.10-1.ppc.rpm</a><br>
 -->
 		<a href="http://packages.debian.org/stable/libs/"
 		        >http://packages.debian.org/stable/libs/</a><br>
@@ -60,8 +60,8 @@ For more information about public key signatures, see
 	<p><span class="title">
 		Win32:
 		</span><br>
-		<a href="release/SDL-1.2.9-win32.zip"
-		                >SDL-1.2.9-win32.zip</a><br>
+		<a href="release/SDL-1.2.10-win32.zip"
+		                >SDL-1.2.10-win32.zip</a><br>
 	</p>
 	<p><span class="title">
 		BeOS:
@@ -73,48 +73,48 @@ For more information about public key signatures, see
 	<p><span class="title">
 		BeOS:
 		</span><br>
-		<a href="release/SDL-1.2.9-x86-beos.zip"
-		                >SDL-1.2.9-x86-beos.zip</a> (BeOS 5.0)<br>
+		<a href="release/SDL-1.2.10-x86-beos.zip"
+		                >SDL-1.2.10-x86-beos.zip</a> (BeOS 5.0)<br>
 	</p>
 -->
 	<p><span class="title">
 		MacOS (Classic):
 		</span><br>
-		<a href="release/SDL-1.2.9-PPC.sea.bin"
-		                >SDL-1.2.9-PPC.sea.bin</a><br>
+		<a href="release/SDL-1.2.10-PPC.sea.bin"
+		                >SDL-1.2.10-PPC.sea.bin</a><br>
 	</p>
 	<p><span class="title">
 		Mac OS X:
 		</span><br>
-		<a href="release/SDL-1.2.9.dmg"
-		                >SDL-1.2.9.dmg</a><br>
+		<a href="release/SDL-1.2.10.dmg"
+		                >SDL-1.2.10.dmg</a><br>
 	</p>
 <!-- Not available yet
 	<p><span class="title">
 		IRIX:
 		</span><br>
-		<a href="http://ftp.mayn.de/pub/irix/games/SDL/SDL-1.2.9_IRIX_N32_mips3.tardist"
-		                                              >SDL-1.2.9_IRIX_N32_mips3.tardist</a><br>
+		<a href="http://ftp.mayn.de/pub/irix/games/SDL/SDL-1.2.10_IRIX_N32_mips3.tardist"
+		                                              >SDL-1.2.10_IRIX_N32_mips3.tardist</a><br>
 	</p>
 	<p><span class="title">
 		OpenBSD:
 		</span><br>
-		<a href="ftp://ftp.openbsd.org/pub/OpenBSD/2.9/packages/i386/sdl-1.2.9-oss.tgz"
-		                                                            >sdl-1.2.9-oss.tgz</a> (i386)<br>
-		<a href="ftp://ftp.openbsd.org/pub/OpenBSD/2.9/packages/powerpc/sdl-1.2.9-oss.tgz"
-		                                                               >sdl-1.2.9-oss.tgz</a> (powerpc)<br>
+		<a href="ftp://ftp.openbsd.org/pub/OpenBSD/2.9/packages/i386/sdl-1.2.10-oss.tgz"
+		                                                            >sdl-1.2.10-oss.tgz</a> (i386)<br>
+		<a href="ftp://ftp.openbsd.org/pub/OpenBSD/2.9/packages/powerpc/sdl-1.2.10-oss.tgz"
+		                                                               >sdl-1.2.10-oss.tgz</a> (powerpc)<br>
 	</p>
 	<p><span class="title">
 		QNX:
 		</span><br>
-		<a href="release/SDL_x86-1.2.9-x86-public.qpr"
-		                >SDL_x86-1.2.9-x86-public.qpr</a><br>
+		<a href="release/SDL_x86-1.2.10-x86-public.qpr"
+		                >SDL_x86-1.2.10-x86-public.qpr</a><br>
 	</p>
 	<p><span class="title">
 		AmigaOS:
 		</span><br>
-		<a href="release/SDL-1.2.9-amiga.zip"
-		                >SDL-1.2.9-amiga.zip</a><br>
+		<a href="release/SDL-1.2.10-amiga.zip"
+		                >SDL-1.2.10-amiga.zip</a><br>
 	</p>
 -->
 </blockquote>
@@ -124,13 +124,13 @@ For more information about public key signatures, see
 	<p><span class="title">
 		Linux:
 		</span><br>
-		<a href="release/SDL-devel-1.2.9-1.i386.rpm"
-		                >SDL-devel-1.2.9-1.i386.rpm</a><br>
+		<a href="release/SDL-devel-1.2.10-1.i386.rpm"
+		                >SDL-devel-1.2.10-1.i386.rpm</a><br>
+		<a href="release/SDL-devel-1.2.10-1.mipsel.rpm"
+		                >SDL-devel-1.2.10-1.mipsel.rpm</a> (Playstation 2)<br>
 <!-- Not available yet
-		<a href="release/SDL-devel-1.2.9-1.mipsel.rpm"
-		                >SDL-devel-1.2.9-1.mipsel.rpm</a> (Playstation 2)<br>
-		<a href="release/SDL-devel-1.2.9-1.ppc.rpm"
-		                >SDL-devel-1.2.9-1.ppc.rpm</a><br>
+		<a href="release/SDL-devel-1.2.10-1.ppc.rpm"
+		                >SDL-devel-1.2.10-1.ppc.rpm</a><br>
 -->
 		<a href="http://packages.debian.org/stable/libdevel/"
 		        >http://packages.debian.org/stable/libdevel/</a><br>
@@ -138,10 +138,10 @@ For more information about public key signatures, see
 	<p><span class="title">
 		Win32:
 		</span><br>
-		<a href="release/SDL-devel-1.2.9-VC6.zip"
-		                >SDL-devel-1.2.9-VC6.zip</a> (Visual C++ 5,6,7)<br>
-		<a href="release/SDL-devel-1.2.9-mingw32.tar.gz"
-		                >SDL-devel-1.2.9-mingw32.tar.gz</a> (Mingw32)<br>
+		<a href="release/SDL-devel-1.2.10-VC6.zip"
+		                >SDL-devel-1.2.10-VC6.zip</a> (Visual C++ 5,6,7)<br>
+		<a href="release/SDL-devel-1.2.10-mingw32.tar.gz"
+		                >SDL-devel-1.2.10-mingw32.tar.gz</a> (Mingw32)<br>
 	</p>
 	<p><span class="title">
 		BeOS:
@@ -153,34 +153,34 @@ For more information about public key signatures, see
 	<p><span class="title">
 		BeOS:
 		</span><br>
-		<a href="release/SDL-devel-1.2.9-x86-beos.zip"
-		                >SDL-devel-1.2.9-x86-beos.zip</a> (BeOS 5.0)<br>
+		<a href="release/SDL-devel-1.2.10-x86-beos.zip"
+		                >SDL-devel-1.2.10-x86-beos.zip</a> (BeOS 5.0)<br>
 	</p>
 -->
 	<p><span class="title">
 		MacOS (Classic):
 		</span><br>
-		<a href="release/SDL-devel-1.2.9-PPC.sea.bin"
-		                >SDL-devel-1.2.9-PPC.sea.bin</a> (MPW + CodeWarrior)<br>
+		<a href="release/SDL-devel-1.2.10-PPC.sea.bin"
+		                >SDL-devel-1.2.10-PPC.sea.bin</a> (MPW + CodeWarrior)<br>
 	</p>
 	<p><span class="title">
 		MacOS X:
 		</span><br>
-		<a href="release/SDL-devel-1.2.9.pkg.tar.gz"
-		                >SDL-devel-1.2.9.pkg.tar.gz</a> (Project Builder + XCode)<br>
+		<a href="release/SDL-devel-1.2.10-extras.dmg"
+		                >SDL-devel-1.2.10-extras.dmg</a> (templates and documentation)<br>
 	</p>
 <!-- Not available yet
 	<p><span class="title">
 		QNX:
 		</span><br>
-		<a href="release/SDL_x86-1.2.9-x86-public.qpr"
-		                >SDL_x86-1.2.9-x86-public.qpr</a><br>
+		<a href="release/SDL_x86-1.2.10-x86-public.qpr"
+		                >SDL_x86-1.2.10-x86-public.qpr</a><br>
 	</p>
 	<p><span class="title">
 		AmigaOS:
 		</span><br>
-		<a href="release/SDL-devel-1.2.9-amiga.zip"
-		                >SDL-devel-1.2.9-amiga.zip</a>
+		<a href="release/SDL-devel-1.2.10-amiga.zip"
+		                >SDL-devel-1.2.10-amiga.zip</a>
 	</p>
 -->
 </blockquote>
