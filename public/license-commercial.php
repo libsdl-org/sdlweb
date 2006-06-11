cfae2edcf5aeb7d63fb9ca63f23bc1c602f05013
SDL Web Management <slouken@libsdl.org>
2006-06-11 23:45:33 +0000

    Yep, alternative commercial license for SDL 1.3

---
+++ public/license-commercial.php
@@ -0,0 +1,19 @@
+<?PHP
+ include ("../include/login.inc.php");
+ include ($header_filename);
+?>
+<h1>Licensing the Simple DirectMedia Layer library</h1>
+
+<blockquote class="graytext">
+
+<p>
+The Simple DirectMedia Layer library will be available under alternative
+commercial terms as of the release of SDL 1.3.
+</p
+
+</blockquote>
+
+<?PHP
+ include ("footer.inc.php");
+?>
+
