52ed360d2026abfdb6c755914f4bb52c4715d2cd
SDL Web Management <slouken@libsdl.org>
2006-06-11 23:45:22 +0000

    Yep, alternative commercial license for SDL 1.3

---
+++ public/license-lgpl.php
@@ -0,0 +1,55 @@
+<?PHP
+ include ("../include/login.inc.php");
+ include ($header_filename);
+?>
+<h1>Licensing the Simple DirectMedia Layer library</h1>
+
+<blockquote class="graytext">
+
+<p>
+The Simple DirectMedia Layer library is currently available under the 
+<a href="http://www.gnu.org/copyleft/lesser.html">
+GNU Lesser General Public License (LGPL)
+</a> version 2.1 or newer.
+</p>
+
+<p>
+SDL's use of the LGPL is twofold:
+</p>
+<ul>
+<li> Allow end users to use custom versions of the library, to support bug
+     fixes and other enhancements.
+<li> Allow any improvements made to the library for one project to benefit the
+     community as a whole.
+</ul>
+
+
+<p>
+To comply with this license, you must give prominent notice that you use
+the Simple DirectMedia Layer library, and that it is included under the
+terms of the LGPL license.  You must provide a copy of the LGPL license.
+<br>
+You must also do <strong>one</strong> of the following:
+</p>
+<ol>
+<li> Link with the library as a shared object (e.g. SDL.dll or libSDL.so)
+<li> Provide the object or source code to your application along with any
+     libraries and custom tools not available with a standard platform
+     development kit.  You may also simply provide a written offer, valid
+     for three years, to provide these materials upon request to anyone
+     with a legal copy of your application.
+</ol>
+
+<p>
+If you include the SDL library in object form, you should also make
+available the source code to the version you provide, including any
+customizations you have made.  If you link to a standard version of
+the library, simply referring to the SDL website is sufficient.
+</p>
+
+</blockquote>
+
+<?PHP
+ include ("footer.inc.php");
+?>
+
