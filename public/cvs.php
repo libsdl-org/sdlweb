5473eace63b2b4affa72dcc335597cb1fb943c81
Sam Lantinga <slouken@libsdl.org>
2007-01-22 05:18:28 +0000

    CVS repository is no longer available

--- public/cvs.php
+++ public/cvs.php
@@ -7,12 +7,6 @@
 <p>
 SDL is now available via <a href="svn.php">Subversion</a>
 </p>
-<p>
-The old CVS repository is browsable here:
-<br>
-<a href="http://www.libsdl.org/cgi/cvsweb.cgi"
-        >http://www.libsdl.org/cgi/cvsweb.cgi</a>
-</p>
 <?PHP
  include ("footer.inc.php");
 ?>
