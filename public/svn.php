4e252dc95f6eadba537394d1cd8961534440f0d8
libsdl.org shell account <slouken@libsdl.org>
2010-04-11 22:08:19 -0700

    Softened the wording a little bit

--- public/svn.php
+++ public/svn.php
@@ -5,10 +5,10 @@
 <h1>SDL Subversion snapshots</h1>
 
 <p>
-SDL is now available via <a href="hg.php">Mercurial</a>. The svn server and
- webview are available for historical purposes (as there are lots of URLs
- and references to svn revision numbers floating around), but development
- does not happen there any more. Use Mercurial instead.
+SDL is now available via <a href="hg.php">Mercurial</a>.
+</p>
+<p>
+The subversion repository and webview are available for historical purposes (as there are lots of URLs and references to svn revision numbers floating around), but development does not happen there any more.
 </p>
 
 <?PHP
