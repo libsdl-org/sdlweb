63e6acd11a3d46be17532d31c61468b70de032fd
libsdl.org shell account <slouken@libsdl.org>
2010-07-04 10:28:26 -0700

    Updated the pages to point to the new wiki

--- public/books.php
+++ public/books.php
@@ -1,45 +1,24 @@
-<?PHP
- include ("../include/login.inc.php");
- include ($header_filename);
-?>
-<h1>Books relating to SDL</h1>
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type"
+content="text/html; charset=iso-8859-1">
+<meta http-equiv="refresh"
+content="3; URL=http://wiki.libsdl.org/moin.cgi/Books">
+<title>This page has moved</title>
+</head>
 
-<blockquote>
-     Erik Yuzwa - August 2006<br>
-     <b><a href="http://learncppbymakinggames.wazooinc.com/">Learn C++ by Making Games</a></b><br>
-     An introduction to C++, using it to develop games with SDL.
-</blockquote>
+<body bgcolor="#FFFFFF">
 
-<blockquote>
-     Erik Yuzwa - December 2005<br>
-     <b><a href="http://book.wazooinc.com/">Game Programming in C++ Start to Finish</a></b><br>
-     An introduction to game programming using SDL and OpenGL on Windows.
-</blockquote>
+<center>
+<H1>This page has moved!</H1>
+</center>
 
-<blockquote>
-     Alberto Garc&iacute;a Serrano - December 2003<br>
-     <b><a href="http://www.ediversitas.com/public-gen.php?isbn=8495836084">Programaci&oacute;n de videojuegos en SDL</a></b><br>
-     Excellent coverage of using SDL in the development of video games.
-</blockquote>
+<P align=center>
+If your browser doesn't automatically take you to the new site,
+please go directly to:
+<a href="http://www.libsdl.org/">http://www.libsdl.org/</a>
+</p>
 
-<blockquote>
-     Ernest Pazera - November 2002<br>
-     <b><a href="http://www.gamedev.net/columns/books/bookdetails.asp?productid=206">Focus On SDL</a></b><br>
-     A good introduction to SDL, SDL_image, SDL_mixer, SDL_ttf, and SDL_net.
-</blockquote>
-
-<blockquote>
-     Ron Penton - November 2002<br>
-     <b><a href="http://www.gamedev.net/columns/books/bookdetails.asp?productid=197">Data Structions for Game Programmers</a></b><br>
-     Coverage of data structures used in game development, with SDL examples.
-</blockquote>
-
-<blockquote>
-     Loki Software, Inc. with John R. Hall - August 2001<br>
-     <b><a href="http://www.nostarch.com/plg.htm">Programming Linux Games</a></b><br>
-     An introduction to programming games on Linux using the SDL library.
-</blockquote>
-
-<?PHP
- include ("footer.inc.php");
-?>
+</body>
+</html>
