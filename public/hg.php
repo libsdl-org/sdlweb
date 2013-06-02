3d98c5edc59f3c164a17872a96e116fbe563b810
libsdl.org shell account <slouken@libsdl.org>
2013-06-02 10:09:17 -0700

    Preparing for the SDL 2.0 release

--- public/hg.php
+++ public/hg.php
@@ -17,28 +17,7 @@ If you're new to Mercurial, check out the great tutorial at <a href="http://hgin
 </p>
 
 <p>
-SDL 1.2:
-<blockquote>
-<pre>
-hg clone -u SDL-1.2 http://hg.libsdl.org/SDL SDL-1.2
-</pre>
-</blockquote>
-</p>
-
-<blockquote>
-<p>
-If you are building SDL with an IDE, you will need to copy the file
-include/SDL_config.h.default to include/SDL_config.h before building.
-</p>
-
-<p>
-If you are building SDL via configure, you will need to run autogen.sh
-before running configure.
-</p>
-</blockquote>
-
-<p>
-SDL 2.0 (<b>WARNING: UNDER CONSTRUCTION</b>):
+SDL 2.0:
 <blockquote>
 <pre>
 hg clone http://hg.libsdl.org/SDL
@@ -46,41 +25,26 @@ hg clone http://hg.libsdl.org/SDL
 </blockquote>
 </p>
 
-<blockquote>
-<p>
-You can build directly from a fresh clone of SDL 2.0.
-</p>
-</blockquote>
-
 <p>
 There is a web interface to the Mercurial repository at:<br>
 <a href="http://hg.libsdl.org/">http://hg.libsdl.org/</a>
 </p>
 
-<hr>
-
-<p><font color="#414141">
-Source snapshot for SDL 1.2 (Updated
-<!-- SDL 1.2 DATE --> Fri Jan 20
-)</p>
-<blockquote>
-<a href="tmp/SDL-1.2.tar.gz">SDL-1.2.tar.gz</a><br/>
-<a href="tmp/SDL-1.2.zip">SDL-1.2.zip</a>
-</blockquote>
+<hr/>
 
 <p><font color="#414141">
 Source snapshot for SDL 2.0 (Updated
-<!-- SDL 2.0 DATE --> Sun Mar 24
+<!-- SDL 2.0 DATE --> Sat Jun  1
 )</p>
-<b>WARNING: UNDER CONSTRUCTION</b>
 <blockquote>
-<a href="tmp/SDL-2.0.tar.gz">SDL-2.0.tar.gz</a><br/>
-<a href="tmp/SDL-2.0.zip">SDL-2.0.zip</a>
+<!-- SDL 2.0 ZIP --><a href="tmp/SDL-2.0.0-7250.zip">SDL-2.0.0-7250.zip</a><!-- -->
+<br/>
+<!-- SDL 2.0 TGZ --><a href="tmp/SDL-2.0.0-7250.tar.gz">SDL-2.0.0-7250.tar.gz</a><!-- -->
 </blockquote>
 
 <strong>Note: These are not official releases and may be unstable!</strong>
 
-<hr>
+<hr/>
 
 <?PHP
  include ("footer.inc.php");
