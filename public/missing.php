81798151a0e4154041c51bcbf37bb4a7c0683b8c
libsdl.org shell account <slouken@libsdl.org>
2013-08-12 09:03:23 -0700

    Added the 404 redirect page

---
+++ public/missing.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>        
+        <title>Simple DirectMedia Layer<?php /* - TITLE */ ?></title>
+        <?php require_once("include/meta.inc.php"); ?><?php $current_page = basename(__FILE__, '.php'); ?>
+    </head>
+    <body>        
+        <div id="wrapper">
+            <?php require_once("include/header.inc.php"); ?>
+            <div id="left">
+                <?php require_once("include/sidebar.inc.php"); ?>
+            </div>
+            <div id="content">
+                <h1>Missing Page</h1>
+                <p>
+                The page you were looking for couldn't be found.
+                </p>
+            </div>
+            <div class="clearer"></div>            
+        </div>
+        <?php require_once("include/footer.inc.php"); ?> 
+
+    </body>
+</html>
