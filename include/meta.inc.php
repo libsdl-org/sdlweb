fa1fa8fe6dbf890accc750b9b04c6ca91d064835
Sam Lantinga <slouken@libsdl.org>
2013-08-18 08:11:19 +0000

    Restored favicon.ico

--- include/meta.inc.php
+++ include/meta.inc.php
@@ -1,4 +1,5 @@
  <meta charset="utf-8" />
+ <link rel="icon" type="image/ico" href="/favicon.ico">
  <link rel="stylesheet" href="/css/fontfacekit/stylesheet.css">
  <link rel="stylesheet" href="/css/stylesheet.css">
  <meta name="keywords" content="SDL, Simple DirectMedia Layer, Download" />
