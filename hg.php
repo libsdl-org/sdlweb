a7a309e9fc6602aadbf47c9a30589c515deeb003
Sam Lantinga <slouken@libsdl.org>
2014-03-08 07:21:09 +0000

    2.0.2 has been released!

--- hg.php
+++ hg.php
@@ -40,12 +40,12 @@ hg clone <a href="http://hg.libsdl.org/SDL">http://hg.libsdl.org/SDL</a>
 
 <p><font color="#414141">
 Source snapshot for SDL 2.0 (Updated
-<!-- SDL 2.0 DATE --> Thu Oct 24
+<!-- SDL 2.0 DATE --> Sat Mar  8
 )</p>
 <blockquote>
-<!-- SDL 2.0 ZIP --><a href="tmp/SDL-2.0.1-7893.zip">SDL-2.0.1-7893.zip</a><!-- -->
+<!-- SDL 2.0 ZIP --><a href="tmp/SDL-2.0.2-8297.zip">SDL-2.0.2-8297.zip</a><!-- -->
 <br/>
-<!-- SDL 2.0 TGZ --><a href="tmp/SDL-2.0.1-7893.tar.gz">SDL-2.0.1-7893.tar.gz</a><!-- -->
+<!-- SDL 2.0 TGZ --><a href="tmp/SDL-2.0.2-8297.tar.gz">SDL-2.0.2-8297.tar.gz</a><!-- -->
 </blockquote>
                   </div>
                 <div class="clearer"></div>            
