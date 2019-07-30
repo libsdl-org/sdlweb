3b702ca475612233ddf64354fa89665beb6b8f5d
Ryan C. Gordon <icculus@icculus.org>
2019-07-30 15:20:01 -0400

    Added an impending-doom-deadline clock.  :)

--- include/nextver.inc.php
+++ include/nextver.inc.php
@@ -1,3 +1,4 @@
 <?php
 $next_sdl_version = '2.0.11';
+$next_sdl_version_duedate = "Oct 31, 2019 23:59:59 GMT-0800"
 ?>
---
+++ next.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>        
+        <?php
+            require_once('include/nextver.inc.php');
+        ?>
+        <title>Simple DirectMedia Layer - Next release's TODO list!</title>
+        <meta http-equiv="Refresh" content="0; url=<?php print($url)?>">
+        <?php require_once("include/meta.inc.php"); ?><?php $current_page = basename(__FILE__, '.php'); ?>
+    </head>
+    <body>        
+        <div id="wrapper">
+            <?php require_once("include/header.inc.php"); ?>
+            <div id="left">
+                <?php require_once("include/sidebar.inc.php"); ?>
+            </div>
+            <div id="content">
+                <h1>SDL <?php print($sdl_next_version); ?> release schedule</h1>
+                    <p>
+                        The next release of SDL is
+                        <strong><?php print($next_sdl_version) ?></strong>.
+                    </p>
+
+                    <p>
+                        Our personal deadline for the release is:
+                        <strong><div id="countdown"/>coming soon!</strong>
+                    </p>
+
+                    <p>
+                        Please help us meet this goal by testing or fixing
+                        issues on the <a href="todo.php"><?php print($next_sdl_version) ?> TODO list</a>!
+                    </p>
+            </div>
+            <div class="clearer"></div>            
+        </div>
+
+        <!-- this code was originally from https://www.w3schools.com/howto/howto_js_countdown.asp
+        <script type="application/javascript">
+            var duedate = "<?php print($next_sdl_version_duedate) ?>";
+            var countDownDate = new Date(duedate).getTime();
+
+            // Update the count down every 1 second
+            var countdown_interval;
+            var interval_function = function() {
+                // Get today's date and time
+                var now = new Date().getTime();
+
+                // Find the distance between now and the count down date
+                var distance = countDownDate - now;
+
+                // Time calculations for days, hours, minutes and seconds
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                // Display the result in the element with id="countdown"
+                var elem = document.getElementById("countdown");
+                if (distance < 0) {
+                    clearInterval(countdown_interval);
+                    distance = now - countDownDate;
+                    days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                    elem.innerHTML = "<font color='#FF0000'>PAST DUE BY " + days + "d " + hours + "h " + minutes + "m " + seconds + "s!"</font>";
+                } else {
+                    elem.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+                }
+            }
+            countdown_interval = setInterval(countdown_function, 1000);
+        </script>
+
+        <?php require_once("include/footer.inc.php"); ?>      
+    </body>
+</html>
+
