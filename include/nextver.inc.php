9af002e719cdeafb2a96f339453ac02cfee72037
Ryan C. Gordon <icculus@icculus.org>
2019-09-20 16:52:36 -0400

    Forgot a semicolon.  :/

--- include/nextver.inc.php
+++ include/nextver.inc.php
@@ -3,7 +3,7 @@
 $current_sdl_major = 2;
 $current_sdl_minor = 0;
 $current_sdl_patch = 10;
-$next_sdl_version_duedate = "Oct 31, 2019 23:59:59 GMT-0800"
+$next_sdl_version_duedate = "Oct 31, 2019 23:59:59 GMT-0800";
 
 // don't touch these.
 $current_sdl_version = "$current_sdl_major.$current_sdl_minor.$current_sdl_patch";
