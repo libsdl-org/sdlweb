9a92fee3f8531c448a7755cf68d71158dbd5fbe0
Ryan C. Gordon <icculus@icculus.org>
2019-09-20 16:42:51 -0400

    Updated version info with new version numbering scheme.

--- include/nextver.inc.php
+++ include/nextver.inc.php
@@ -1,4 +1,12 @@
 <?php
-$next_sdl_version = '2.0.11';
+// Update these on each release.
+$current_sdl_major = 2;
+$current_sdl_minor = 0;
+$current_sdl_patch = 10;
 $next_sdl_version_duedate = "Oct 31, 2019 23:59:59 GMT-0800"
+
+// don't touch these.
+$current_sdl_version = "$current_sdl_major.$current_sdl_minor.$current_sdl_patch";
+$development_sdl_version = "$current_sdl_major.$current_sdl_minor." . ($current_sdl_patch + 1);
+$next_sdl_version = "$current_sdl_major.$current_sdl_minor." . ($current_sdl_patch + 2);
 ?>
