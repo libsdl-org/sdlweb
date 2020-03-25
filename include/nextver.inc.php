dc5679c1350a96217f5f9c56461dcc823781f468
Ryan C. Gordon <icculus@icculus.org>
2020-03-25 14:13:02 -0400

    Whoops, this value is current version, not next release.

--- include/nextver.inc.php
+++ include/nextver.inc.php
@@ -2,7 +2,7 @@
 // Update these on each release.
 $current_sdl_major = 2;
 $current_sdl_minor = 0;
-$current_sdl_patch = 14;
+$current_sdl_patch = 12;
 $next_sdl_version_duedate = "Jun 30, 2020 23:59:59 GMT-0800";
 
 // don't touch these.
