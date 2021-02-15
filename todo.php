631528dbb8f9043023f8aa0a20a0e9c81efcbc33
Ryan C. Gordon <icculus@icculus.org>
2021-02-15 17:35:30 +0000

    Fix todo.php to point to the current GitHub milestone.

--- todo.php
+++ todo.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>        
+    <head>
         <?php
             require_once('include/nextver.inc.php');
-            // This URL should always work, assuming you've created the appropriate "target-X.Y.Z" keywork in Bugzilla and tagged some bugs with it.
-            $url = "https://bugzilla.libsdl.org/buglist.cgi?bug_status=UNCONFIRMED&bug_status=NEW&bug_status=WAITING&bug_status=RESPONDED&bug_status=ASSIGNED&bug_status=REOPENED&keywords=target-$next_sdl_version&keywords_type=allwords&known_name=$next_sdl_version%20TODO&order=Bug%20Number&query_format=advanced";
+            $url = "https://github.com/libsdl-org/SDL/milestone/$github_milestone_id";
         ?>
         <title>Simple DirectMedia Layer - Next release's TODO list!</title>
         <meta http-equiv="Refresh" content="0; url=<?php print($url)?>">
         <?php require_once("include/meta.inc.php"); ?><?php $current_page = basename(__FILE__, '.php'); ?>
     </head>
-    <body>        
+    <body>
         <div id="wrapper">
             <?php require_once("include/header.inc.php"); ?>
             <div id="left">
@@ -30,9 +29,9 @@
                         instead.
                     </p>
             </div>
-            <div class="clearer"></div>            
+            <div class="clearer"></div>
         </div>
-        <?php require_once("include/footer.inc.php"); ?>      
+        <?php require_once("include/footer.inc.php"); ?>
     </body>
 </html>
 
