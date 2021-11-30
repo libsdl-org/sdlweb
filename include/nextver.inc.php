1b7dba53b3b8adeee5479b4323af83587bff65fe
Sam Lantinga <slouken@libsdl.org>
2021-11-30 17:23:18 +0000

    SDL 2.0.18 has been released!

--- include/nextver.inc.php
+++ include/nextver.inc.php
@@ -2,9 +2,9 @@
 // Update these on each release.
 $current_sdl_major = 2;
 $current_sdl_minor = 0;
-$current_sdl_patch = 16;
-$next_sdl_version_duedate = "Nov 20, 2021 23:59:59 GMT-0800";
-$github_milestone_id = '2';   // sadly, these don't have version strings, so we have to update for each release.
+$current_sdl_patch = 18;
+$next_sdl_version_duedate = "Mar 1, 2022 23:59:59 GMT-0800";
+$github_milestone_id = '3';   // sadly, these don't have version strings, so we have to update for each release.
 
 // don't touch these.
 $current_sdl_version = "$current_sdl_major.$current_sdl_minor.$current_sdl_patch";
