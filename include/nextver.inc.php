d5fe61003e9648085789b6be66091c97f3ae5a60
Ryan C. Gordon <icculus@icculus.org>
2021-12-17 22:04:27 +0000

    Bumped nextver to deal with shortened 2.0.20 milestone.

--- include/nextver.inc.php
+++ include/nextver.inc.php
@@ -3,8 +3,10 @@
 $current_sdl_major = 2;
 $current_sdl_minor = 0;
 $current_sdl_patch = 18;
-$next_sdl_version_duedate = "Mar 1, 2022 23:59:59 GMT-0800";
-$github_milestone_id = '3';   // sadly, these don't have version strings, so we have to update for each release.
+$next_sdl_version_duedate = "Jan 7, 2022 23:59:59 GMT-0800";
+
+// for 2.0.20 we added a new milestone, so this number is out of order for the next few versions. Don't just bump it by one!
+$github_milestone_id = '5';   // sadly, these don't have version strings, so we have to update for each release.
 
 // don't touch these.
 $current_sdl_version = "$current_sdl_major.$current_sdl_minor.$current_sdl_patch";
