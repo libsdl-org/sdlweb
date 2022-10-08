e79c5c4edbeb41ae32ba2d7e5ac30058a129567d
Sam Lantinga <slouken@libsdl.org>
2022-10-08 16:38:30 +0000

    Updated the stable branch notice with the 2.24.1 release

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,5 +1,5 @@
 <div id="header">                
     <div id="download_hint" class="box">
-        Get the current <strong>stable</strong> <a href="https://github.com/libsdl-org/SDL/releases/latest">SDL version 2.24.0</a><br />
+        Get the current <strong>stable</strong> <a href="https://github.com/libsdl-org/SDL/releases/latest">SDL version 2.24.1</a><br />
     </div>
 </div>  
