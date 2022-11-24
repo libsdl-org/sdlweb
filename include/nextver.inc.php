85a52cf40f1d10186d3c391e1eae1c17b1a8cc08
Sam Lantinga <slouken@libsdl.org>
2022-11-24 22:07:11 +0000

    Updated for the 2.26.0 release

--- include/nextver.inc.php
+++ include/nextver.inc.php
@@ -1,11 +1,11 @@
 <?php
 // Update these on each release.
 $current_sdl_major = 2;
-$current_sdl_minor = 24;
+$current_sdl_minor = 26;
 $current_sdl_patch = 0;
-$next_sdl_version_duedate = "November 1, 2022 23:59:59 GMT-0800";
+$next_sdl_version_duedate = "March 1, 2023 23:59:59 GMT-0800";
 
-$github_milestone_id = '7';   // sadly, these don't have version strings, so we have to update for each release.
+$github_milestone_id = '8';   // sadly, these don't have version strings, so we have to update for each release.
 
 // don't touch these.
 $current_sdl_version = "$current_sdl_major.$current_sdl_minor.$current_sdl_patch";
