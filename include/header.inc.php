68852b6cf26a0e8e25d4cfbda529f76d60b85a3d
Sam Lantinga <slouken@libsdl.org>
2023-03-07 00:18:55 +0000

    Updated for the 2.26.4 release

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,5 +1,5 @@
 <div id="header">                
     <div id="download_hint" class="box">
-        Get the current <strong>stable</strong> <a href="https://github.com/libsdl-org/SDL/releases/latest">SDL version 2.26.3</a><br />
+        Get the current <strong>stable</strong> <a href="https://github.com/libsdl-org/SDL/releases/latest">SDL version 2.26.4</a><br />
     </div>
 </div>  
