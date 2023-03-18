04e749bf06766b93d186eba7028366cd9f036e0b
Ryan C. Gordon <icculus@icculus.org>
2023-03-18 13:04:51 -0400

    Added a gdc2023 page.

---
+++ gdc2023/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>Simple DirectMedia Layer - GDC2023</title>
+       <?php require_once("../include/meta.inc.php"); ?><?php $current_page = basename(__FILE__, '.php'); ?>
+    </head>
+    <body class="home">
+        <div id="wrapper">
+            <?php require_once("../include/header.inc.php"); ?>
+            <div id="left">
+                <?php require_once("../include/sidebar.inc.php"); ?>
+            </div>
+            <div id="content">
+            <p><h1>SDL at Game Developers Conference 2023</h1></p>
+            <p><ul>
+              <li>(Final slides coming soon!)</li>
+              <li><a href="https://github.com/libsdl-org/SDL">SDL's GitHub page</a></li>
+              <li><a href="https://github.com/libsdl-org/SDL_shader_tools">SDL_shader_tools (the shader compiler)'s GitHub page</a></li>
+              <li><a href="https://github.com/libsdl-org/SDL/blob/4a6528e3f0091f44142f3e4cd7fe14eb7660735a/test/testvulkan.c">Example Vulkan program from the talk</a></li>
+              <li><a href="https://github.com/icculus/SDL/blob/c8e167878b268a971f1839cbe2b590f82be11dce/test/testgpu_simple_clear.c">Example SDL GPU API program from the talk</a></li>
+            </ul></p>
+        </div>
+        <?php require_once("../include/footer.inc.php"); ?>
+    </body>
+</html>
