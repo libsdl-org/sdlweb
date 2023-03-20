e8d42d59646d665bc8d99cdb99dcbd9ee78ecb03
Sam Lantinga <slouken@libsdl.org>
2023-03-20 16:51:09 +0000

    Updated GDC presentation

--- gdc2023/index.php
+++ gdc2023/index.php
@@ -11,13 +11,17 @@
                 <?php require_once("../include/sidebar.inc.php"); ?>
             </div>
             <div id="content">
-            <p><h1>SDL at Game Developers Conference 2023</h1></p>
+            <p><h1><a href="https://schedule.gdconf.com/session/open-source-game-development-summit-simple-directmedia-layer-past-present-and-future/892854">SDL at Game Developers Conference 2023</a></h1></p>
             <p><ul>
-              <li>(Final slides coming soon!)</li>
-              <li><a href="https://github.com/libsdl-org/SDL">SDL's GitHub page</a></li>
+	      <li><a href="GDC23_Presentation_SDL.pptx">Presentation Slides</a></li>
+              <li><a href="https://github.com/libsdl-org/SDL">SDL 3.0 Source Code</a></li>
+              <li><a href="https://github.com/libsdl-org/SDL/tree/main/docs/README-migration.md">SDL 3.0 Transition Guide</a></li>
+              <li><a href="https://gist.github.com/slouken/7c41d29a252d58778794311f555ebe8c">Code example, accumulating floating point relative mouse motion</a></li>
+              <li><a href="https://gist.github.com/slouken/146fda70e9c919422c70731378ef882d">Code example, getting the desktop scale for a UI scaling factor</a></li>
+              <li><a href="https://github.com/libsdl-org/SDL_shader_tools/tree/main/docs">SDL 3.0 GPU documentation</a></li>
+              <li><a href="https://github.com/libsdl-org/SDL/blob/4a6528e3f0091f44142f3e4cd7fe14eb7660735a/test/testvulkan.c">Simple 3D example, Vulkan version</a></li>
+              <li><a href="https://github.com/icculus/SDL/blob/c8e167878b268a971f1839cbe2b590f82be11dce/test/testgpu_simple_clear.c">Simple 3D example, SDL version</a></li>
               <li><a href="https://github.com/libsdl-org/SDL_shader_tools">SDL_shader_tools (the shader compiler)'s GitHub page</a></li>
-              <li><a href="https://github.com/libsdl-org/SDL/blob/4a6528e3f0091f44142f3e4cd7fe14eb7660735a/test/testvulkan.c">Example Vulkan program from the talk</a></li>
-              <li><a href="https://github.com/icculus/SDL/blob/c8e167878b268a971f1839cbe2b590f82be11dce/test/testgpu_simple_clear.c">Example SDL GPU API program from the talk</a></li>
             </ul></p>
         </div>
         <?php require_once("../include/footer.inc.php"); ?>
