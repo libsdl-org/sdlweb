cbec53bffcc55df9d7870957d057c8bffc30608b
Sam Lantinga <slouken@libsdl.org>
2024-10-14 15:53:33 +0000

    Added a separate SDL3 language bindings page

--- languages.php
+++ languages.php
@@ -11,160 +11,28 @@
             <div id="left">
                 <?php require_once("include/sidebar.inc.php"); ?>
             </div>
-            <div id="content"><h1>SDL Language Bindings</h1>
-                  <p class="intro">
-                            The Simple DirectMedia Layer library has bindings to many different
-                            programming languages.  While SDL is written in C, it works well with
-                            C++ and many people like to use it with various scripting languages
-                            and special purpose programming languages.
-                        </p>
-                
-               
+            <div id="content"><h1>SDL 3.0 Language Bindings</h1>
+                <p class="intro">
+                        The Simple DirectMedia Layer library has bindings to many different
+                        programming languages.  While SDL is written in C, it works well with
+                        C++ and many people like to use it with various scripting languages
+                        and special purpose programming languages.
+                </p>
+                <p>
+                        SDL 2.0 language bindings are available <a href="languages-2.0.php">here</a>.
+                </p>
+
                 <div class="clearer"></div>
                 <div class="col left">
                    
                     <blockquote>
                         <ul>
                             <li> <strong>
-                                    Ada
-                                </strong>
-                                <br/>
-                                SDLAda -
-                                <a href="https://github.com/ada-game-framework/sdlada">https://github.com/ada-game-framework/sdlada</a>
-                            </li>
-			    <li> <strong>
-                                    Beeflang
-                                </strong>
-                                <br/>
-                                BeefLibs SDL2 -
-                                <a href="https://github.com/beefytech/Beef/tree/master/BeefLibs/SDL2">https://github.com/beefytech/Beef/tree/master/BeefLibs/SDL2</a>
-                            </li>
-                            <li> <strong>
-                                    C#
-                                </strong>
-                                <br/>
-                                SDL2# -
-                                <a href="https://github.com/flibitijibibo/SDL2-CS">https://github.com/flibitijibibo/SDL2-CS</a>
-                            </li>
-                            <li> <strong>
                                     D
                                 </strong>
                                 <br/>
-                                DerelictSDL2 -
-                                <a href="https://derelictorg.github.io/packages/sdl2/">https://derelictorg.github.io/packages/sdl2/</a>
-                <br/>
                                 BindBC-SDL -
                                 <a href="https://github.com/BindBC/bindbc-sdl">https://github.com/BindBC/bindbc-sdl</a>
-                            </li>
-                            <li> <strong>
-                                    Go
-                                </strong>
-                                <br/>
-                                go-sdl2 -
-                                <a href="https://github.com/veandco/go-sdl2">https://github.com/veandco/go-sdl2</a>
-                            </li>
-                            <li> <strong>
-                                    Haskell
-                                </strong>
-                                <br/>
-                                sdl2 -
-                                <a href="https://hackage.haskell.org/package/sdl2">https://hackage.haskell.org/package/sdl2</a>
-                            </li>
-                            <li> <strong>
-                                    Hollywood
-                                </strong>
-                                <br/>
-                                RebelSDL -
-                                <a href="https://www.hollywood-mal.com/docs/html/rebelsdl/">https://www.hollywood-mal.com/docs/html/rebelsdl/</a>
-                            </li>
-                            <li> <strong>
-                                    Lua
-                                </strong>
-                                <br/>
-                                Lua-SDL2 -
-                                <a href="https://github.com/Tangent128/luasdl2">https://github.com/Tangent128/luasdl2</a>
-                            </li>
-                            <li> <strong>
-                                    OCaml
-                                </strong>
-                                <br/>
-                                Tsdl -
-                                <a href="https://erratique.ch/software/tsdl">https://erratique.ch/software/tsdl</a>
-                            </li>
-                            <li> <strong>
-                                    Odin
-                                </strong>
-                                <br/>
-                                Odin SDL 2 -
-                                <a href="https://github.com/odin-lang/Odin/tree/master/vendor/sdl2">https://github.com/odin-lang/Odin/tree/master/vendor/sdl2</a>
-                            </li>
-                            <li> <strong>
-                                    Pascal
-                                </strong>
-                                <br/>
-                                Bare Game -
-                                <a href="https://github.com/sysrpl/Bare.Game/blob/master/source/bare.interop.sdl2.pas">https://github.com/sysrpl/Bare.Game/blob/master/source/bare.interop.sdl2.pas</a>
-                                <br/>
-                                Pascal SDL 2 -
-                                <a href="https://github.com/ev1313/Pascal-SDL-2-Headers">https://github.com/ev1313/Pascal-SDL-2-Headers</a>
-                                <br/>
-                                SDL2 for Pascal -
-                                <a href="https://github.com/PascalGameDevelopment/SDL2-for-Pascal">https://github.com/PascalGameDevelopment/SDL2-for-Pascal</a>
-                            </li>
-                            <li> <strong>
-                                    Python
-                                </strong>
-                                <br/>
-                                pygame_sdl2 -
-                                <a href="https://github.com/renpy/pygame_sdl2">https://github.com/renpy/pygame_sdl2</a>
-				<br/>
-                                PySDL2 -
-                                <a href="https://pypi.org/project/PySDL2/">https://pypi.org/project/PySDL2/</a>
-				<br/>
-                                pysdl2-cffi -
-                                <a href="https://pythonhosted.org/pysdl2-cffi/">https://pythonhosted.org/pysdl2-cffi/</a>
-                            </li>
-                            <li> <strong>
-                                    Rust
-                                </strong>
-                                <br/>
-                                Rust-SDL2 -
-                                <a href="https://github.com/Rust-SDL2/rust-sdl2">https://github.com/Rust-SDL2/rust-sdl2</a>
-                            </li>
-                            <li> <strong>
-                                    TypeScript
-                                </strong>
-                                <br/>
-                                SDL_ts -
-                                <a href="https://github.com/smack0007/SDL_ts">https://github.com/smack0007/SDL_ts</a>
-                            </li>
-                            <li> <strong>
-                                    Zig
-                                </strong>
-                                <br/>
-                                SDL on Zig -
-                                <a href="https://github.com/pwbh/SDL">https://github.com/pwbh/SDL</a>
-                            </li>
-                            <li> <strong>
-                                    V
-                                </strong>
-                                <br/>
-                                SDL for Vlang -
-                                <a href="https://github.com/vlang/sdl">https://github.com/vlang/sdl</a>
-                            </li>
-                            <li> <strong>
-                                    Nim
-                            </strong>
-                            <br/>
-                                SDL for Nim -
-                                <a href="https://github.com/nim-lang/sdl2">https://github.com/nim-lang/sdl2</a>
-                            </li>
-                            <li> <strong>
-                                Crystal
-                            </strong>
-                            <br/>
-                                SDL for Crystal -
-                                <a href="https://github.com/ysbaddaden/sdl.cr">https://github.com/ysbaddaden/sdl.cr</a>
                             </li>
                         </ul>
                     </blockquote>
