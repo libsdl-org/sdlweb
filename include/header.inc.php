69e8aab975c3e52829bb5780fa6dccc97b2e0a53
Sam Lantinga <slouken@libsdl.org>
2024-11-01 22:43:50 +0000

    Updated for the latest SDL release

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,6 +1,6 @@
 <div id="header">                
     <div id="download_hint" class="box">
-        Get the current <strong>stable</strong> <a href="https://github.com/libsdl-org/SDL/releases/latest">SDL version 2.30.8</a><br />
-        Get the SDL 3.0 preview! <a href="https://github.com/libsdl-org/SDL/releases/tag/preview-3.1.3">SDL version 3.1.3</a><br />
+        Get the current <strong>stable</strong> <a href="https://github.com/libsdl-org/SDL/releases/latest">SDL version 2.30.9</a><br />
+        Get the SDL 3.0 preview! <a href="https://github.com/libsdl-org/SDL/releases/tag/preview-3.1.6">SDL version 3.1.6</a><br />
     </div>
 </div>  
--- include/nextver.inc.php
+++ include/nextver.inc.php
@@ -1,9 +1,9 @@
 <?php
 // Update these on each release.
 $current_sdl_major = 3;
-$current_sdl_minor = 0;
+$current_sdl_minor = 2;
 $current_sdl_patch = 0;
-$next_sdl_version_duedate = "January 1, 2024 23:59:59 GMT-0800";
+$next_sdl_version_duedate = "January 1, 2025 23:59:59 GMT-0800";
 
 $github_milestone_id = '10';   // sadly, these don't have version strings, so we have to update for each release.
 
