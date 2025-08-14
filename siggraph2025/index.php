af4244affe59ff6f9cb426cde70cf01dba0fce5a
Ryan C. Gordon <icculus@icculus.org>
2025-08-14 19:22:43 +0000

    siggraph2025: Added the SDL GPU Panel details.

---
+++ siggraph2025/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>Simple DirectMedia Layer - SIGGRAPH 2025</title>
+       <?php require_once("../include/meta.inc.php"); ?><?php $current_page = basename(__FILE__, '.php'); ?>
+    </head>
+    <body class="home">
+        <div id="wrapper">
+            <?php require_once("../include/header.inc.php"); ?>
+            <div id="left">
+                <?php require_once("../include/sidebar.inc.php"); ?>
+            </div>
+            <div id="content">
+            <p><h1>SDL at SIGGRAPH 2025</h1></p>
+            <p>Evan Hemsley, the SDL GPU Project Lead, participated in a panel
+               at SIGGRAPH 2025, titled "Using the SDL GPU Graphics API in Education."</p>
+            <p>The content of the discussion is available here.</p>
+            <p><ul>
+	      <li><a href="SIGGRAPH_2025_SDL_GPU_Panel.mp3">Audio from the panel in MP3 format.</a></li>
+	      <li><a href="https://wiki.libsdl.org/SDL3/SIGGRAPH2025PanelTranscript">The transcript of the panel.</a></li>
+              <li><a href="https://github.com/libsdl-org/SDL">SDL 3.0 Source Code</a></li>
+            </ul></p>
+        </div>
+        <?php require_once("../include/footer.inc.php"); ?>
+    </body>
+</html>
+
